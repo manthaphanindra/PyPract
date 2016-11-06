3656e2b88906bd4616e900345c4a2d3b3c9d964f
Phanindra <mantha.phanindra@gmail.com>
2016-11-06 17:02:17 +0400

    added swap program

---
+++ swap.py
@@ -0,0 +1,10 @@
+# swapping of two no's:
+def input(x,y):
+    temp=x
+    x=y
+    y=temp
+    # .format() function is used to 
+    print 'The value of x after swapping is: {}'.format(x)
+    print 'The value of y after swapping is: {}'.format(y)
+input(3,6)
+input(9,2)
